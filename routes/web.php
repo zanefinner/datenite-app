b96bc5ffec67d8315427afb9fa79b9ac4c8e5451
Zane Finner <zanefinner@pop-os.localdomain>
2020-04-30 13:36:23 -0400

    Dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check())
+    {
+        return redirect('/dashboard');
+    }else
+    {
+        return view('auth.register');
+    }
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index');
